fa96a8054b97d79bf3dbf9c903e629a6a4d1790e
omerdr7 <omerfarukdere32@gmail.com>
2024-06-26 12:16:36 +0300

    First switch method created to add new word into the game but there are some bugs in the method.

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -1,10 +1,13 @@
 package hangman;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Hangman {
 
     static Scanner s = new Scanner(System.in);
+    static ArrayList<String> wordList = new ArrayList<>();
+    static String newWord = null;
 
     public static void mainMenu() {
         System.out.println("Welcome to hangman game.Enjoyyy!");
@@ -19,18 +22,59 @@ public class Hangman {
         if (chose > 0 && chose < 5) {
             switch (chose) {
                 case 1:
-
+                    addWord();
                     break;
                 default:
 
             }
         } else {
             System.out.println("Please select a valid number.");
+            mainMenu();
         }
     }
 
-    public static void main(String[] args) {
+    public static void addWord() {
+        System.out.println("Enter the word which you want to add to game:");
+        newWord = s.next();
+        int control = 0;
+        if (newWord.length() <= 3) {
+            System.out.println("This word is too short.You should give more than 3 letter.");
+            //this if check is word more than 3 letter.
+            control++;
+        }
+        if (newWord.length() > 3) {
+            for (String b : wordList) {
+                if (newWord.equalsIgnoreCase(b)) {
+                    System.out.println("You cannot add a word two time.");
+                    //this if check is word added before.
+                }
 
+            }
+            control++;
+        }
+        if (newWord.length() > 3) {
+            for (char c : newWord.toCharArray()) {
+                if (!Character.isDigit(c)) {
+                    System.out.println("You cannot enter a word which is include digit.");
+                    //this if check is word include digit.
+                }
+            }
+            control++;
+        }
+        if (control == 3) {
+            System.out.println("Word is added.");
+            wordList.add(newWord);
+        } else {
+            System.out.println("Word cannot add.");
+        }
+        //This case(1) check all conditions for example is word include digit.
+
+        mainMenu();
+
+    }
+
+    public static void main(String[] args) {
+        mainMenu();
     }
 
 }
