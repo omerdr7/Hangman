762a6e67ab840245eeba884d57666d8aff580879
omerdr7 <omerfarukdere32@gmail.com>
2024-06-26 12:00:31 +0300

    Main method cleaned.

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -4,19 +4,17 @@ import java.util.Scanner;
 
 public class Hangman {
 
+    static Scanner s = new Scanner(System.in);
+
     public static void mainMenu() {
+        System.out.println("Welcome to hangman game.Enjoyyy!");
+        System.out.println("Please select a number ");
+
         System.out.print("1-" + "add a word\n");
         System.out.print("2-" + "new game\n");
         System.out.print("3-" + "show scores\n");
         System.out.print("4-" + "exit\n");
         System.out.println("Select a number:");
-    }
-
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Welcome to hangman game.Enjoyyy!");
-
-        System.out.println("Please select a number ");
         int chose = s.nextInt();
         if (chose > 0 && chose < 5) {
             switch (chose) {
@@ -29,6 +27,9 @@ public class Hangman {
         } else {
             System.out.println("Please select a valid number.");
         }
+    }
+
+    public static void main(String[] args) {
 
     }
 
